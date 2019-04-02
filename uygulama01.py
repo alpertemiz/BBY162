ccc3462b8683b2b0124f0eba8777469a4d468b68
alpertemiz <48517728+alpertemiz@users.noreply.github.com>
2019-04-02 23:48:23 +0300

    Add files via upload

---
+++ uygulama01.py
@@ -0,0 +1,54 @@
+__author__ = "ALPER TEMİZ"
+#Test Uygulaması
+print("test uygulamamız başlıyor...")
+print("Aşağıdaki soruları doğru cevaplamaya çalışınız")
+soru1 = ["Ahtopotların 8 kalbi vardır. [D/Y]"]
+cevap1 = ["D"]
+puan = 5
+#soru1
+cevap = input((soru1[0]))
+if cevap1[0] == cevap.upper ():
+    print("Cevap doğru, tebrikler.")
+    puan = 5
+else:
+    print("Cevap yanlış.")
+soru2 = ["Türkiye 4 coğrafi bölgeden oluşmaktadır. [D/Y]"]
+cevap2 = ["Y"]
+puan = 5
+#soru2
+cevap = input((soru2[0]))
+if cevap2[0] == cevap.upper ():
+    print("Cevap doğru, tebrikler.")
+    puan = 5
+else:
+    print("Cevap yanlış.")
+soru3 = ["1 saat 60 dakikadır. [D/Y]"]
+cevap3 = ["D"]
+puan = 5
+# soru3
+cevap = input((soru3[0]))
+if cevap3[0] == cevap.upper():
+    print("Cevap doğru, tebrikler.")
+    puan = 5
+else:
+    print("Cevap yanlış.")
+soru4 = ["Ekmek karbonhidrat kaynağıdır. [D/Y]"]
+cevap4 = ["D"]
+puan = 5
+# soru4
+cevap = input((soru4[0]))
+if cevap4[0] == cevap.upper():
+    print("Cevap doğru, tebrikler.")
+    puan = 5
+else:
+    print("Cevap yanlış.")
+soru5 = ["En iyi iletken gümüşdür. [D/Y]"]
+cevap5 = ["D"]
+puan = 5
+# soru5
+cevap = input((soru5[0]))
+if cevap5[0] == cevap.upper():
+    print("Cevap doğru, tebrikler.")
+    puan = 5
+else:
+    print("Cevap yanlış.")
